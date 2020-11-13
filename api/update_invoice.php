4c04152995f6e0c95d13936a5284af4f2f0460ec
Winnie <0433034@nkust.edu.tw>
2020-11-13 11:04:59 +0800

    完成編輯發票

---
+++ api/update_invoice.php
@@ -0,0 +1,22 @@
+<?php
+include_once "../base.php";
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+$sql="update 
+  invoice 
+set 
+  `code`='{$_POST['code']}',
+  `number`='{$_POST['number']}',
+  `date`='{$_POST['date']}',
+  `payment`='{$_POST['payment']}',
+where
+  `id`='{$_POST['id']}'";
+
+$pdo->exce($sql);
+
+header("location:../index.php?do=invoice_list");
+
+?>
