6010e79c6e5d216623ed9626f2ade05aa287fc37
Winnie <0433034@nkust.edu.tw>
2020-11-20 14:24:18 +0800

    增加all()函式取得多筆資料

---
+++ test.php
@@ -0,0 +1,74 @@
+<?php
+include_once "base.php";
+
+// 取得單一資料自訂函式
+function find($table,$id){
+  global $pdo;
+  $sql="select *from $table where";
+  if(!is_array($id)){
+
+    $sql=$sql."id='$id'";
+
+    }else{
+
+      foreach($id as $key=>$value){
+        $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        // $tmp[]="`".$key."`='".$value."'";
+      }
+      $sql=$sql.implode(' && ',$tmp);
+
+    }
+    
+    $row=$pdo->query($sql)->fetch();
+    return $row;
+}
+
+
+function all($table,...$arg){
+  global $pdo;
+  // gettype($arg);
+  // echo gettype($arg);
+
+  $sql="select * from $table";
+  
+if(isset($arg[0])
+
+  if(is_array($arg[0])){
+    
+    // 製作會在where後面的句子字串(陣列格式)
+    foreach($arg[0] as $key=>$value){
+      $tmp[]=sprintf("`%s`='%s'",$key,$value);
+      // $tmp[]="`".$key."`='".$value."'";
+    }
+
+    $sql=$sql."where".implode(' && ',$tmp);
+  
+  }else{
+
+    if(!isset()){}
+    // 製作非陣列的語句接在$sql後面
+    $sql=$sql.$arg[0];
+    
+  }
+  
+  if(isset($arg[1])){
+
+    $sql=$sql.$arg[1];
+  
+  }
+
+  echo $sql."<br>";
+  return $pdo->query($sql)->fetchAll();
+
+}
+
+
+echo "<hr>";
+print_r(all('invoice'));
+echo "<hr>";
+print_r(all('invoice'),['code'=>'ET','number'=>'12597846'
+]);
+
+
+
+?>
