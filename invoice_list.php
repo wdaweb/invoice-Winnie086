2bd271e55996a8e34542fc64b0148f0abe5eb0ef
Winnie <0433034@nkust.edu.tw>
2020-12-01 01:22:37 +0800

    完成發票各期分頁，但是連結功能還未完成

--- invoice_list.php
+++ invoice_list.php
@@ -1,32 +1,34 @@
 <?php
 include_once "base.php";
 
-
 if(isset($_GET['period'])){
-    
     $period=$_GET['period'];
-    
 }else {
-    
     $period=ceil(date("m")/2);
-    
 } 
 
 if(isset($_GET['year'])){
-    
     $year=$_GET['year'];
-    
 }else {
-    
     $year=date("Y");
-    
 } 
 
 
 $sql="select * from `invoice` where period='$period' order by date desc ";
 $rows=$pdo->query($sql)->fetchAll();
+// print_r($rows);
 
-
+$data_nums=$pdo->query("select count(`invoice`.`id`) from `invoice` where year(`date`)='$year' && `period`='$period'")->fetch();
+$per = 20; //每頁顯示項目數量
+$pages = ceil($data_nums[0]/$per); //無條件進入法，算總共需要幾頁
+if (!isset($_GET["page"])){ //假如$_GET["page"]未設置
+    $page=1; //剛開始進來，則在此設定起始頁數
+}else {
+    $page = intval($_GET["page"]); //確認頁數只能夠是數值資料，顯示現在在第幾頁
+}
+$start = ($page-1)*$per; //每一頁開始的資料序號，資料庫中的排法跟陣列一樣，索引值從第0筆開始
+$result=$pdo->query($sql.' LIMIT '.$start.','.$per)->fetchAll();//LIMIT函式:$start顯示每頁開始的索引值、$per代表每頁要顯示幾筆資料
+// print_r($result);
 ?>
 
 
@@ -37,10 +39,6 @@ $rows=$pdo->query($sql)->fetchAll();
     </form>
     </div>
 
-    <!-- <div class="row justify-content-center align-items-center">
-        <a href="?year=<?=$year-1;?>" class="col-6"><i class="fas fa-angle-double-left"></i></a>
-        <a href="?year=<?=$year;?>" class="col-6"><i class="fas fa-angle-double-right"></i></a>
-    </div> -->
 
     <div class="row justify-content-around" style="list-style-type:none;paddin:0">
     <li><a href="?do=invoice_list&period=1">1-2月</a></li>
@@ -49,26 +47,31 @@ $rows=$pdo->query($sql)->fetchAll();
     <li><a href="?do=invoice_list&period=4">7-8月</a></li>
     <li><a href="?do=invoice_list&period=5">9-10月</a></li>
     <li><a href="?do=invoice_list&period=6">11-12月</a></li>
-    <!-- <li><a href="?do=invoice_list&year=<?=$year;?>&period=6">11-12月</a></li> -->
     </div>
 
 <table class="table text-center">
     <tr>
+        <td>編號</td>
         <td>發票號碼</td>
         <td>消費日期</td>
         <td>消費金額</td>
         <td>操作</td>
     </tr>
 
-    <?php
+    <?php 
 
-    
-    foreach($rows as $row){
-    ?>
-    <tr>
-        <td><?=$row['code']."-".$row['number'];?></td>
-        <td><?=$row['date'];?></td>
-        <td><?=$row['payment'];?></td>
+    foreach($result as $row){
+        
+        $code=$row['code']."-".$row['number'];
+        $date=$row['date'];
+        $payment=$row['payment'];
+        ?>
+        
+        <td></td>
+        <td><?php echo $code; ?></td>
+        <td><?php echo $date;?></td>
+        <td><?php echo $payment;?></td>
+         
         <td>
             <button class="btn btn-sm btn-dark">
                 <a href="?do=edit_invoice&id=<?=$row['id'];?>" class="text-white"> 編輯</a>
@@ -87,3 +90,23 @@ $rows=$pdo->query($sql)->fetchAll();
 </table>
 
 
+<div class="container row justify-content-center">
+<?php
+    //分頁頁碼
+    echo "<div class='col-12'>";
+    echo '共 '.$data_nums[0].' 筆-現在在第 '.$page.' 頁-共 '.$pages.' 頁';
+    echo "</div>";
+    
+    
+    echo "<div class='col-12'>";
+    echo "<br><a href=?page=1>首頁</a> ";
+    echo "第";
+    for( $i=1 ; $i<=$pages ; $i++ ){
+        if ( $page-3 < $i && $i < $page+3 ){
+            echo "<a href=?page=".$i.">".$i."</a> ";
+        }
+    } 
+    echo " 頁 <a href=?page=".$pages.">末頁</a><br /><br />";
+    echo "</div>";
+?>
+</div>
--- pagecut.php
+++ pagecut.php
@@ -7,21 +7,21 @@
     $period=ceil(date("m")/2);
     $sql = "SELECT * FROM `invoice` WHERE `period`='$period' ORDER BY `date` desc";
     
-    // $data_nums = mysql_num_rows($result); //統計總筆數
-    // $data_nums=$pdo->query($sql)->fetchAll();
+    // $data_nums = mysql_num_rows($result); //統計在該年中各期的總筆數
+    // $data_nums=$pdo->query()->fetch();
     $data_nums=$pdo->query("select count(`invoice`.`id`) from `invoice` where year(`date`)='$year' && `period`='$period'")->fetch();
-    print_r($data_nums[0]);
+    // print_r($data_nums[0]);
 
-    $per = 10; //每頁顯示項目數量
-    $pages = ceil($data_nums[0]/$per); //無條件進入法
+    $per = 50; //每頁顯示項目數量
+    $pages = ceil($data_nums[0]/$per); //無條件進入法，算總共需要幾頁
     if (!isset($_GET["page"])){ //假如$_GET["page"]未設置
-        $page=1; //則在此設定起始頁數
+        $page=1; //剛開始進來，則在此設定起始頁數
     } else {
-        $page = intval($_GET["page"]); //確認頁數只能夠是數值資料
+        $page = intval($_GET["page"]); //確認頁數只能夠是數值資料，顯示現在在第幾頁
     }
-    $start = ($page-1)*$per; //每一頁開始的資料序號
-    $result=$pdo->query($sql.' LIMIT '.$start.','.$per)->fetchAll();
-    print_r($result);
+    $start = ($page-1)*$per; //每一頁開始的資料序號，資料庫中的排法跟陣列一樣，索引值從第0筆開始
+    $result=$pdo->query($sql.' LIMIT '.$start.','.$per)->fetchAll();//LIMIT函式:$start顯示每頁開始的索引值、$per代表每頁要顯示幾筆資料
+    // print_r($result);
     // $result = mysql_query($sql.' LIMIT '.$start.', '.$per,$conn) or die("Error");
 ?>
 
@@ -35,18 +35,15 @@
 <?php
 //輸出資料內容
 
-// while ($row = mysql_fetch_array ($result)){
 foreach($result as $row){
     
-    $code=$row['code'];
-    $number=$row['number'];
+    $code=$row['code']."-".$row['number'];
     $date=$row['date'];
     $payment=$row['payment'];
     ?>
     
     <tr class="text-align:center;">
         <td><?php echo $code; ?></td>
-        <td><?php echo $number;?></td>
         <td><?php echo $date;?></td>
         <td><?php echo $payment;?></td>
     </tr>
@@ -60,7 +57,7 @@ foreach($result as $row){
 
 <?php
     //分頁頁碼
-    echo '共 '.$data_nums[0].' 筆-在 '.$page.' 頁-共 '.$pages.' 頁';
+    echo '共 '.$data_nums[0].' 筆-現在在第 '.$page.' 頁-共 '.$pages.' 頁';
     echo "<br /><a href=?page=1>首頁</a> ";
     echo "第 ";
     for( $i=1 ; $i<=$pages ; $i++ ) {
